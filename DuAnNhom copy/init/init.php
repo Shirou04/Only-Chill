b52e4678b1991e21f3c57c108cca17de225c5938
trungggggggggggg <trunglqps40671@gmail.com>
2024-11-21 22:45:36 +0700

    Delete init.php

--- DuAnNhom copy/init/init.php
+++
@@ -1,6 +0,0 @@
-<?php 
-include "config.php";
-session_start();
-$baseurl="http://localhost/Web204_DuAnMau/fstudio-WeddingDresses";
-$action=$_GET['action']??"";
-include "controllers/controller.php";
