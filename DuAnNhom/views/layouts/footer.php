7f1e1ad52c6a2c4fb2e926531aeb0701515a6ac2
trungggggggggggg <trunglqps40671@gmail.com>
2024-11-22 10:04:34 +0700

    Create footer.php

---
+++ DuAnNhom/views/layouts/footer.php
@@ -0,0 +1,78 @@
+<!-- Star Footer -->
+<footer>
+        <div class="mail">
+            <h1>Theo dõi và cập nhật tin tức mới nhất</h1>
+            <p>Vinh hạnh của chúng tôi là mang đến cho bạn những chuyến đi đáng nhớ. Mang đến cho bạn những chuyến đi đầy
+                cảm hứng. Khám phá những vùng đất mới. Tự do khám phá cùng chúng tôi.</p>
+            <div class="enter">
+                <input class="enter-your-email" type="text" placeholder="Nhập email của bạn">
+                <div class="follow-email"><a class="follow" href="">Theo dõi</a></div>
+            </div>
+        </div>
+        <div class="section">
+            <!-- 1 -->
+            <div class="cot1 lienhe">
+                <h2>Thông tin liên hệ</h2>
+                <p>
+                    <a href="" class="chudam">Địa chỉ</a><br>
+                    abc, phường xyz, quận OKX, Thành phố BCD
+                </p>
+                <p>
+                    <a href="" class="chudam">Email</a><br>
+                    support@onlychill.com
+                </p>
+                <p>
+                    <a href="" class="chudam">Hotline</a><br>
+                    1900 1899
+                </p>
+                <p>
+                    <a href="" class="chudam">Thời gian hỗ trợ</a><br>
+                    08:30 - 21:30 các ngày trong tuần
+                </p>
+            </div>
+            <!-- 2 -->
+            <div class="cot1 huongdan">
+                <h2>Hướng dẫn</h2>
+                <p><a href="index.html">Trang chủ</a></p>
+                <p><a href="">Giới thiệu</a></p>
+                <p></p><a href="">Tour du lịch</a></p>
+                <p><a href="">Tin tức</a></p>
+                <p><a href="">Liên hệ</a></p>
+            </div>
+            <!-- 3 -->
+            <div class="cot1 canbiet">
+                <h2>Thông tin cần biết</h2>
+                <p><a href="">Về chúng tôi</a></p>
+                <p><a href="">Câu hỏi thường gặp</a></p>
+                <p></p><a href="">Điều kiện, điều khoản</a></p>
+                <p><a href="">Quy chế hoạt động</a></p>
+                <p><a href="">Liên hệ</a></p>
+            </div>
+            <!-- 4 -->
+            <div class="cot1 ketnoi">
+                <h2>Kết nối</h2>
+                <div class="icon-mxh">
+                    <img src="../image/facebook.png" alt="">
+                    <img src="../image/youtube.png" alt="">
+                    <img src="../image/twitter.png" alt="">
+                    <img src="../image/instagram.png" alt="">
+                    <img src="../image/google.png" alt="">
+                </div>
+                <br>
+                <h2>Tải ứng dụng Onlychill</h2>
+                <div class="icon-dowload">
+                    <img src="../image/ios.png" alt="">
+                    <img src="../image/android.png" alt="">
+                </div>
+                <br>
+                <h2>Thanh toán</h2>
+                <div class="icon-pay">
+                    <img src="../image/vnpay.png" alt="">
+                    <img src="../image/jcb.png" alt="">
+                    <img src="../image/visa.png" alt="">
+                    <img src="../image/shoppepay.png" alt="">
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer -->
