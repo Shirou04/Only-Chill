68066a5d0500e139e3eebf485eded8fe9250b65a
trungggggggggggg <trunglqps40671@gmail.com>
2024-11-22 10:04:20 +0700

    Create header.php

---
+++ DuAnNhom/views/layouts/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/header_footer.css">
+    <link rel="stylesheet" href="../css/all.css">
+    <link rel="stylesheet" href="../css/home.css">
+    <title>Trang chủ</title>
+</head>
+<style>
+    body{
+        margin: auto;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+</style>
+<body>
+    <!-- Star Header -->
+    <header>
+        <div class="head">
+            <div class="logo">
+                <img src="../image/Logo.png" alt="">
+            </div>
+            <div class="menu">
+                <div class="menu-con"><a class="page" href="index.html">Trang chủ</a></div>
+                <div class="menu-con"><a class="page" href="">Giới thiệu</a></div>
+                <div class="menu-con"><a class="page" href="pages/detail.php">Tour du lịch</a></div>
+                <div class="menu-con"><a class="page" href="">Tin tức</a></div>
+                <div class="menu-con"><a class="page" href="">Liên hệ</a></div>
+            </div>
+            <div class="icon-header">
+                <img class="icon-head" src="../image/loupe.png" alt="">
+                <img class="icon-head" src="../image/user.png" alt="">
+                <a href="signup.html"><img class="icon-head" src="../image/headphones.png" alt=""></a>
+            </div>
+        </div>
+        <br><hr>
+        
+        <div class="banner_header">
+            <img src="../image/banner01.jpg" alt="" width="100%">
+        </div>
+    </header>
+    <!-- End Header -->
