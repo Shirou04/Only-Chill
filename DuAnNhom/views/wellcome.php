311572abdd703295e389b1576a72db7b624b4edd
trungggggggggggg <trunglqps40671@gmail.com>
2024-11-22 10:03:58 +0700

    Create wellcome.php

---
+++ DuAnNhom/views/wellcome.php
@@ -0,0 +1,218 @@
+<?php require_once "layouts/header.php"?>
+<!-- Star Mid -->
+<main>
+       <h2 class="why">Vì sao bạn nên chọn Only Chill?</h2>
+       <div class="reasons">
+            <div class="reason">
+                <img src="../image/suachua.png" alt=""><br>
+                <p>Sửa chữa và đảm bảo lịch trình</p>
+            </div>
+            <div class="reason">
+                <img src="../image/lienhe24.png" alt=""><br>
+                <p>Liên hệ trong vòng 24 giờ</p>
+            </div>
+            <div class="reason">
+                <img src="../image/diadiem.png" alt=""><br>
+                <p>Địa điểm cụ thể của lịch trình</p>
+            </div>
+       </div>
+       <div class="tour_in_water">
+            <h2 class="tour_water">Tour trong nước</h2>
+            <p>Hơn 1000 tour đa dạng giá hời giới hạn</p>
+       </div>
+
+        <!-- |||||||||||||||||||||||||||||| -->
+              
+       <div class="locations">
+        <!-- 1 -->
+        <div class="location">
+            <img src="../image/vungtau.png" alt="">
+            <div class="content">
+                <h2>Vũng Tàu</h2>
+                <p>"Vũng Tàu, thành phố ven biển nổi tiếng của Việt Nam, là điểm nhấn 
+                    trong chuyến đi 6 ngày 5 đêm của tôi. Nổi bật với những bãi biển 
+                    xanh mát và cảnh quan tuyệt đẹp, Vũng Tàu mang đến...</p>
+                <p><i class="fe-star">4,5</i>                    
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star-half-stroke"></i>
+                </p>
+                <p class="clock">
+                    <img src="../image/clock.png" alt="">
+                    <a>6 ngày, 5 đêm</a>
+                </p>
+                <p class="x-money">
+                    <a class="money">4.500.000 - 6.500.000</a>
+                    <a class="vnd">VNĐ</a>
+                </p>
+            </div>
+        </div>
+
+        <!-- 2 -->
+        <div class="location">
+            <img src="../image/hanoi.png" alt="">
+            <div class="content">
+                <h2>Hà Nội</h2>
+                <p>"Hà Nội, thủ đô nhộn nhịp của Việt Nam, là điểm nhấn trong chuyến đi 
+                    4 ngày 3 đêm của tôi. Với lịch sử phong phú và văn hóa sôi động, 
+                    Hà Nội mang đến sự pha trộn độc đáo giữa cái cũ và cái mới... </p>
+                <p><i class="fe-star">4,5</i>                    
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star-half-stroke"></i>
+                </p>
+                <p class="clock">
+                    <img src="../image/clock.png" alt="">
+                    <a>6 ngày, 5 đêm</a>
+                </p>
+                <p class="x-money">
+                    <a class="money">4.500.000 - 6.500.000</a>
+                    <a class="vnd">VNĐ</a>
+                </p>
+            </div>
+        </div>
+
+        <!-- 3 -->
+        <div class="location">
+            <img src="../image/phuquoc.png" alt="">
+            <div class="content">
+                <h2>Phú Quốc</h2>
+                <p>"Phú Quốc, thiên đường nhiệt đới ở miền Nam Việt Nam, là điểm nhấn 
+                    trong chuyến đi 7 ngày 6 đêm của tôi. Những bãi biển cát trắng và 
+                    nước biển trong xanh là nơi lý tưởng để bơi lội và lặn ngắm san hô...</p>
+                <p><i class="fe-star">4,5</i>                    
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star-half-stroke"></i>
+                </p>
+                <p class="clock">
+                    <img src="../image/clock.png" alt="">
+                    <a>6 ngày, 5 đêm</a>
+                </p>
+                <p class="x-money">
+                    <a class="money">4.500.000 - 6.500.000</a>
+                    <a class="vnd">VNĐ</a>
+                </p>
+            </div>
+        </div>
+
+        <!-- 4 -->
+        <div class="location">
+            <img src="../image/dalat.png" alt="">
+            <div class="content">
+                <h2>Đà Lạt</h2>
+                <p>"Đà Lạt, thành phố quyến rũ ở cao nguyên miền Trung Việt Nam, là điểm nhấn 
+                    trong chuyến đi 5 ngày 4 đêm của tôi. Nổi tiếng với khí hậu mát mẻ và cảnh 
+                    quan lãng mạn, Đà Lạt mang đến tầm nhìn tuyệt đẹp... </p>
+                <p><i class="fe-star">4,5</i>                    
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star-half-stroke"></i>
+                </p>
+                <p class="clock">
+                    <img src="../image/clock.png" alt="">
+                    <a>6 ngày, 5 đêm</a>
+                </p>
+                <p class="x-money">
+                    <a class="money">4.500.000 - 6.500.000</a>
+                    <a class="vnd">VNĐ</a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+   <h1 class="top">Sự kiện sắp diễn ra</h1>
+   <p>Tổng hợp các sự kiện đáng chú ý sắp diễn ra trên khắp thế giới</p>
+   <!-- 1 -->
+   <div class="events">
+        <div class="event">
+            <div><img src="../image/y.png" alt=""></div>
+            <div>
+                <h3>Carnival of Venice (Carnival di Venezia) - Ý</h3>
+                <p>27/1 - 13/2, 2024</p>
+                <a>Lễ hội Carnival of Venice nổi tiếng với những chiếc mặt nạ 
+                độc đáo và trang phục lộng lẫy. Sự kiện này bao gồm nhiều 
+                hoạt động như diễu hành, buổi hòa nhạc...</a>
+                <button class="xemthem">Xem Thêm</button>
+            </div>
+        </div>
+        <!-- 2 -->
+        <div class="event">
+            <div class="img"><img src="../image/duc.png" alt=""></div>
+            <div>
+                <h3>Oktoberfest - Đức</h3>
+                <p>21/9 - 6/10, 2024</p>
+                <a>Oktoberfest là lễ hội bia lớn nhất thế giới, diễn ra tại Munich. 
+                    Du khách có thể thưởng thức nhiều loại bia, ẩm thực đặc trưng 
+                    và tham gia vào các hoạt động văn hóa...</a>
+                <button class="xemthem">Xem Thêm</button>
+            </div>
+        </div>
+        <!-- 3 -->
+        <div class="event">
+            <div><img src="../image/Ando.png" alt=""></div>
+            <div>
+                <h3>Holi Festival - Ấn Độ</h3>
+                <p>25/3, 2024</p>
+                <a>Lễ hội Holi, hay còn gọi là Lễ hội màu sắc, là một trong những
+                     lễ hội lớn nhất ở Ấn Độ, kỷ niệm sự chiến thắng của cái tốt 
+                     trước cái ác...</a>
+                <button class="xemthem">Xem Thêm</button>
+            </div>
+        </div>
+        <!-- 4 -->
+        <div class="event">
+            <div><img src="../image/taybannha.png" alt=""></div>
+            <div>
+                <h3>La Tomatina - Tây Ban Nha</h3>
+                <p>27/1 - 13/2, 2024</p>
+                <a>La Tomatina là một lễ hội nổi tiếng ở Buñol, nơi du khách tham 
+                    gia vào cuộc chiến ném cà chua. Đây là một sự kiện vui vẻ, thu 
+                    hút hàng ngàn người...</a>
+                <button class="xemthem">Xem Thêm</button>
+            </div>
+        </div>
+   </div>
+
+   <h1 class="top">Điểm đến yêu thích</h1>
+   <p>Tổng hợp địa điểm được đánh giá cao</p>
+    <div class="country-x-three">
+        <div class="country">
+            <img src="../image/Combo 1.png" alt="">
+        </div>
+        <div class="country">
+            <img src="../image/Combo 2.png" alt="">
+        </div>
+        <div class="country">
+            <img src="../image/Combo 3.png" alt="">
+        </div>
+    </div>
+    <div class="nations">
+        <img src="../image/hanoi2.png" alt="">
+        <img src="../image/bangkok.png" alt="">
+        <img src="../image/phuquoc2.png" alt="">
+        <img src="../image/japan.png" alt="">
+        <img src="../image/phap.png" alt="">
+        <img src="../image/danang.png" alt="">
+    </div>
+
+    <h1 class="top donghanh">Đồng hành cùng Only Chill</h1>
+    <div class="sponsor"> 
+        <img src="../image/sponsor1.png" alt="">
+        <img src="../image/sponsor2.png" alt="">
+        <img src="../image/sponsor3.png" alt="">
+        <img src="../image/sponsor4.png" alt="">
+        <!-- <img src="../image/sponsor5.png" alt=""> -->
+        <img src="../image/sponsor6.png" alt="">
+    </div>
+    </main>
+    <!-- End Mid -->
+    <?php require_once "layouts/footer.php"?>
